b72e464e9293f84c68bb0fadca29152366b8252a
Laurence Hartgill <laurence@hartgill.co.uk>
2024-11-30 14:59:34 +0000

    fix: Newline handling of test failure messages in run output log

--- src/results-handlers/util.ts
+++ src/results-handlers/util.ts
@@ -1,2 +1,5 @@
+import { EOL } from 'os';
+
 /* eslint-disable import-x/prefer-default-export */
-export const lines = (...text: string[]) => text.join('\r\n') + '\r\n';
+export const lines = (...text: string[]) =>
+  text.flatMap(line => line.split(EOL)).join('\r\n') + '\r\n';
